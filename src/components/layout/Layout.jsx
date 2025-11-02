46bef2f053c3453dc4656a9022acacb84cc1a5ec
Rashedul Alam <rashed464@outlook.com>
2025-11-02 21:35:31 +0600

    page added

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,9 +1,18 @@
-inport React from 'react';
+import React from 'react';  
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-function Layout({ children }) {
+function Layout() {
   return (
-    <div className="layout">
-      {children}
+     <div style={{display: "flex", flexDirection: "column", minHeight: "100vh"}}>
+      <Header />     
+      <Navbar />
+      <main style={{ flex: 1, padding: "20px" }}>
+        <Outlet /> {/* The current page (Home/About/Contact) will render here */}
+      </main>
+      <Footer />
     </div>
   );
 }
